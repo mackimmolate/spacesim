b56929ade4d4b885b3d29f99f1c26d0023b7d699
mackimmolate <150170389+mackimmolate@users.noreply.github.com>
2026-01-27 12:13:51 +0100

    Update Vite config base path and remove test settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/',
-  test: {
-    environment: 'node',
-    include: ['src/**/*.test.ts']
-  }
+  base: "/spacesim/",
 });
